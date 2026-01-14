3d6088bc17914ae520a2064813a611307929f3a7
Brennan <bahrbm@byu.edu>
2026-01-13 22:15:16 -0700

    Implemented basic move functionality by following along with video lecture from gitHub.

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -30,4 +30,8 @@ public class ChessPosition {
     public int getColumn() {
         return col;
     }
+    @Override
+    public String toString() {
+        return String.format("[%d,%d]",row,col);
+    }
 }
