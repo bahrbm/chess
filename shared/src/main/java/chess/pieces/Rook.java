991df8d2d85f1be02e354da3b9334943edb73719
Brennan Bahr <brennan.m.bahr@gmail.com>
2026-02-18 11:33:26 -0700

    Simplified Rook Calc to be much more readable

--- shared/src/main/java/chess/pieces/Rook.java
+++ shared/src/main/java/chess/pieces/Rook.java
@@ -10,10 +10,12 @@ public class Rook implements MoveCalc {
     private final ChessBoard board;
     private final ChessPosition startPosition;
     private final Collection<ChessMove> validMoves = new LinkedList<>();
+    private final ChessGame.TeamColor myTeam;
 
     public Rook(ChessBoard board, ChessPosition startPosition){
         this.board = board;
         this.startPosition = startPosition;
+        this.myTeam = board.getPiece(startPosition).getTeamColor();
     }
 
 
@@ -21,115 +23,111 @@ public class Rook implements MoveCalc {
     public Collection<ChessMove> findMoves() {
 
         // Get the current piece so we can get the color, and get the adjusted row and column for indexing
-        ChessPiece myPiece = board.getPiece(startPosition);
         int row = startPosition.getRow();
         int col = startPosition.getColumn();
 
-        Collection<ChessMove> botMoves = new LinkedList<>();
-        Collection<ChessMove> leftMoves = new LinkedList<>();
-        boolean topPieceFound = false;
-        boolean rightPieceFound = false;
-
-        // Go column by column
-        for(int i = 1; i < 9; i++){
-
-            // if you are on the same column as the rook, go from bottom to top
-            if(i == col){
-                for(int j = 1; j < 9; j++){
-                    ChessPosition currPos = new ChessPosition(j,i);
-
-                    // Skip the rook
-                    if(j == row){
-                        continue;
-                    }
-
-                    //Check below the rook
-                    if(j < row){
-
-                        //If there isn't a piece add it to valid moves and botMoves
-                        if(board.getPiece(currPos)==null){
-                            ChessMove move = new ChessMove(startPosition, currPos, null);
-                            validMoves.add(move);
-                            botMoves.add(move);
-                        }
-                        // If there is a piece and it's the other team, capture and delete all moves below
-                        else if(board.getPiece(currPos).getTeamColor() != myPiece.getTeamColor()){
-                            ChessMove move = new ChessMove(startPosition, currPos, null);
-                            validMoves.removeAll(botMoves);
-                            validMoves.add(move);
-                            botMoves.add(move);
-                        }
-                        // If there is a piece and it's our teams, delete all moves below
-                        else{
-                            validMoves.removeAll(botMoves);
-                        }
-                    }
-                    // Check above the rook
-                    else{
-                        if(!topPieceFound){
-                            //If there isn't a piece add it to valid moves and botMoves
-                            if(board.getPiece(currPos)==null){
-                                ChessMove move = new ChessMove(startPosition, currPos, null);
-                                validMoves.add(move);
-                            }
-                            // If there is a piece and it's the other team, capture and delete all moves above
-                            else if(board.getPiece(currPos).getTeamColor() != myPiece.getTeamColor()){
-                                ChessMove move = new ChessMove(startPosition, currPos, null);
-                                validMoves.add(move);
-                                topPieceFound = true;
-                            }
-                            // If there is a piece and it's our teams, delete all moves above
-                            else{
-                                topPieceFound = true;
-                            }
-                        }
-                    }
-                }
-            }
-            // Looking to the left of the rook
-            else if(i < col){
-                // Same row as the rook, but different column
-                ChessPosition currPos = new ChessPosition(row,i);
-                //If there isn't a piece add it to valid moves and botMoves
-                if(board.getPiece(currPos)==null){
-                    ChessMove move = new ChessMove(startPosition, currPos, null);
-                    validMoves.add(move);
-                    leftMoves.add(move);
-                }
-                // If there is a piece and it's the other team, capture and delete all moves above
-                else if(board.getPiece(currPos).getTeamColor() != myPiece.getTeamColor()){
-                    ChessMove move = new ChessMove(startPosition, currPos, null);
-                    validMoves.removeAll(leftMoves);
-                    validMoves.add(move);
-                    leftMoves.add(move);
-                }
-                // If there is a piece and it's our teams, delete all moves above
-                else{
-                    validMoves.removeAll(leftMoves);
-                }
-            }
-            // Check to the right of the rook
-            else{
-                ChessPosition currPos = new ChessPosition(row,i);
-                if(!rightPieceFound){
-                    //If there isn't a piece add it to valid moves and botMoves
-                    if(board.getPiece(currPos)==null){
-                        ChessMove move = new ChessMove(startPosition, currPos, null);
-                        validMoves.add(move);
-                    }
-                    // If there is a piece and it's the other team, capture and delete all moves above
-                    else if(board.getPiece(currPos).getTeamColor() != myPiece.getTeamColor()){
-                        ChessMove move = new ChessMove(startPosition, currPos, null);
-                        validMoves.add(move);
-                        rightPieceFound = true;
-                    }
-                    // If there is a piece and it's our teams, delete all moves above
-                    else{
-                        rightPieceFound = true;
-                    }
-                }
-            }
-        }
+        // Check each direction and add moves as needed
+        checkRight(row,col);
+        checkLeft(row,col);
+        checkAbove(row,col);
+        checkBelow(row,col);
         return validMoves;
+    }
+
+    void checkRight(int row, int col){
+
+        int currCol = col + 1;
+        // increment column until we find a piece
+        while(currCol < 9){
+
+            ChessPosition currPos = new ChessPosition(row,currCol);
+
+            //Check if the space is empty
+            if(board.getPiece(currPos)==null){
+                validMoves.add(new ChessMove(startPosition,currPos,null));
+            }
+            // If there is a piece there but it is the other team's then capture
+            else if(board.getPiece(currPos).getTeamColor()!=myTeam){
+                validMoves.add(new ChessMove(startPosition,currPos,null));
+                return;
+            }
+            else{
+                return;
+            }
+
+            currCol += 1;
+        }
+    }
+
+    void checkLeft(int row, int col){
+        int currCol = col - 1;
+        // increment column until we find a piece
+        while(currCol > 0){
+
+            ChessPosition currPos = new ChessPosition(row,currCol);
+
+            //Check if the space is empty
+            if(board.getPiece(currPos)==null){
+                validMoves.add(new ChessMove(startPosition,currPos,null));
+            }
+            // If there is a piece there but it is the other team's then capture
+            else if(board.getPiece(currPos).getTeamColor()!=myTeam){
+                validMoves.add(new ChessMove(startPosition,currPos,null));
+                return;
+            }
+            else{
+                return;
+            }
+
+            currCol -= 1;
+        }
+    }
+
+    void checkAbove(int row, int col){
+        int currRow = row + 1;
+        // increment column until we find a piece
+        while(currRow < 9){
+
+            ChessPosition currPos = new ChessPosition(currRow,col);
+
+            //Check if the space is empty
+            if(board.getPiece(currPos)==null){
+                validMoves.add(new ChessMove(startPosition,currPos,null));
+            }
+            // If there is a piece there but it is the other team's then capture
+            else if(board.getPiece(currPos).getTeamColor()!=myTeam){
+                validMoves.add(new ChessMove(startPosition,currPos,null));
+                return;
+            }
+            else{
+                return;
+            }
+
+            currRow += 1;
+        }
+    }
+
+    void checkBelow(int row, int col){
+        int currRow = row - 1;
+        // increment column until we find a piece
+        while(currRow > 0){
+
+            ChessPosition currPos = new ChessPosition(currRow,col);
+
+            //Check if the space is empty
+            if(board.getPiece(currPos)==null){
+                validMoves.add(new ChessMove(startPosition,currPos,null));
+            }
+            // If there is a piece there but it is the other team's then capture
+            else if(board.getPiece(currPos).getTeamColor()!=myTeam){
+                validMoves.add(new ChessMove(startPosition,currPos,null));
+                return;
+            }
+            else{
+                return;
+            }
+
+            currRow -= 1;
+        }
     }
 }
