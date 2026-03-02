5978180bcb2774d5291822c19ed80266e75f5381
Brennan <bahrbm@byu.edu>
2026-03-01 22:27:49 -0700

    Finished ClearServiceTest

--- server/src/test/java/service/ClearServiceTest.java
+++ server/src/test/java/service/ClearServiceTest.java
@@ -1,6 +1,5 @@
 package service;
 
-import chess.ChessGame;
 import dataaccess.*;
 import model.*;
 import org.junit.jupiter.api.Test;
@@ -9,21 +8,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ClearServiceTest {
 
-    private GameDAO gameDAO = new MemoryGameDAO();
-    private UserDAO userDAO = new MemoryUserDAO();
-    private AuthDAO authDAO = new MemoryAuthDAO();
-    private ClearService service = new ClearService(userDAO,authDAO,gameDAO);
+    private final GameDAO gameDAO = new MemoryGameDAO();
+    private final UserDAO userDAO = new MemoryUserDAO();
+    private final AuthDAO authDAO = new MemoryAuthDAO();
+    private final ClearService service = new ClearService(userDAO,authDAO,gameDAO);
 
     @Test
     void clear() {
         UserData user = new UserData("a","A","a@mail");
         AuthData auth = new AuthData("a","asdfghjk;lasdf");
-        GameData game = new GameData(1,"white","black","best game",new ChessGame());
 
         userDAO.createUser(user);
         authDAO.createAuth(auth);
         gameDAO.addGame("best game");
 
         service.clear();
+
+        assertNull(userDAO.findByUsername("a"));
+        assertNull(authDAO.findByAuthToken("asdfghjk;lasdf"));
+        assertEquals(0, gameDAO.getAllGames().size());
     }
 }
